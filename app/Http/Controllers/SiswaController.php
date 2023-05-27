aa9171373f81806b1166fc9cdb0bf947201a2037
Armand96 <armand.iskandar96@gmail.com>
2023-05-27 10:24:01 +0700

    init

---
+++ app/Http/Controllers/SiswaController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Siswa;
+use Illuminate\Http\Request;
+
+class SiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $siswas = Siswa::all();
+        return response()->json($siswas);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $request->validate([
+            'nama_siswa' => 'required',
+            'kelas' => 'required',
+            'tgl_lahir' => 'required'
+        ]);
+
+        $data = $request->all();
+
+        Siswa::create($data);
+
+        return response()->json(['message' => 'Siswa Berhasil Disimpan']);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Siswa $siswa)
+    {
+        return response()->json($siswa);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Siswa $siswa)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Siswa $siswa)
+    {
+        $data = $request->all();
+        $siswa->update($data);
+        return response()->json(['message' => 'Siswa Berhasil Diperbarui']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Siswa  $siswa
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Siswa $siswa)
+    {
+        $siswa->delete();
+        return response()->json(['message'=>'Siswa Berhasil Dihapus']);
+    }
+}
